4899ae74355767e7efbb36178857480211ecd225
levil664 <56757711+levil664@users.noreply.github.com>
2020-05-08 15:56:30 +0500

    ЗАПАРА с крестиком , МИША ПОМОГИ!!!!!!!!!!!!!!!!КАААААААААААААААААААААААААНСЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЛ

--- static/main-static/js/FUNC.js
+++ static/main-static/js/FUNC.js
@@ -2,3 +2,8 @@ $('#adding-post').on('click', function(event) {
         $('#modal1').toggleClass('open');
         $('#modal2').toggleClass('open');
     });
+
+$('#modal2').on('click', function(event) {
+        $('#modal1').toggleClass('open');
+        $('#modal2').toggleClass('open');
+    });
