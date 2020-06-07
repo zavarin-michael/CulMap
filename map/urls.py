0eb3cd79b38a16ce03de34bfb89c209af35db106
zavarin-michael <33755295+zavarin-michael@users.noreply.github.com>
2020-06-07 11:41:20 +0500

    Начал реализовывать вывод все сообщений у определенной метки

--- map/urls.py
+++ map/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.map),
+    path(r'', views.Map),
 ]
--- map/views.py
+++ map/views.py
@@ -5,10 +5,20 @@ from about.models import Comment
 from map.models import MapMarks
 from django.http import JsonResponse
 
-def map(request):
+def Map(request):
     if request.method == "GET":
         if request.is_ajax():
             mark = MapMarks.objects.filter(id = request.GET["id"]).values()
+            s = str(mark.first()['id_comment'])
+
+            spis = s.split('_')
+
+            commentslist = []
+            for i in spis:
+                if (i != ''):
+                    a = Comment.objects.filter(id = int(i)).values().first()
+                    commentslist.append(a)
+
             return JsonResponse(mark.first())
         else:
             marks = MapMarks.objects.all().values('id', 'position_x', 'position_y')
@@ -20,8 +30,8 @@ def map(request):
         comment.comment = data.data["comment"]
         comment.username = data.data["username"]
         comment.id_mark = data.data["id_mark"]
-        mark = MapMarks.objects.filter(id = data.data["id_mark"]).values()
-        mark.id_commnet = mark.id_comment + str(data.data["id_mark"]) + str('_')
+        mark = MapMarks.objects.get(id = data.data["id_mark"])
+        mark.id_comment = mark.id_comment + str(data.data["id_mark"]) + '_'
         mark.save()
         comment.save()
 
