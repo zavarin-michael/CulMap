dd1324e6455653448f94dfb7e275f84d7eee25aa
levil664 <56757711+levil664@users.noreply.github.com>
2020-06-16 11:23:55 +0500

    плюсик (добавление марки)

--- static/main-static/js/FUNC.js
+++ static/main-static/js/FUNC.js
@@ -7,3 +7,14 @@ $('#Layer_1').on('click', function(event) {
         $('#modal1').toggleClass('open');
         $('#modal2').toggleClass('open');
 });
+
+$('#adding_mark').on('click', function(event) {
+        $('#modal3').toggleClass('open');
+        $('#modal2').toggleClass('open');
+});
+
+$('#Layer_2').on('click', function(event) {
+        $('#modal3').toggleClass('open');
+        $('#modal2').toggleClass('open');
+});
+
