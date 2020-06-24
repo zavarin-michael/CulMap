0543789d22e54ea165201b413a4a66a489d36cf5
zavarin-michael <33755295+zavarin-michael@users.noreply.github.com>
2020-06-24 13:37:27 +0500

    Верстка двух форм

--- static/main-static/js/FUNC.js
+++ static/main-static/js/FUNC.js
@@ -10,11 +10,11 @@ $('#Layer_1').on('click', function(event) {
 
 $('#adding_mark').on('click', function(event) {
         $('#modal3').toggleClass('open');
-        $('#modal2').toggleClass('open');
+        $('#modal4').toggleClass('open');
 });
 
 $('#Layer_2').on('click', function(event) {
         $('#modal3').toggleClass('open');
-        $('#modal2').toggleClass('open');
+        $('#modal4').toggleClass('open');
 });
 
